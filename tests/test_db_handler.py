9818ba88f55b422705180719584907b40f825066
Piotr Szlachetka <popek24b@gmail.com>
2026-02-08 19:15:02 +0100

    Add test for db_handler

---
+++ tests/test_db_handler.py
@@ -0,0 +1,28 @@
+from core.handlers.db_handler import DBHandler
+from core.events import PresenceStartEvent, PresenceEndEvent
+
+def test_db_handler_start_and_end(monkeypatch):
+    
+    fake_start_called = []
+    fake_end_called = []
+
+    def fake_log_start(name):
+        fake_start_called
+        return 123
+    
+    def fake_log_end(event_id, snapshot_path):
+        fake_end_called
+
+    monkeypatch.setattr("coroe.handlers.db_handler.log_presence_start", fake_log_start)
+    monkeypatch.setattr("core.handlers.db_handler.log_presence_end", fake_log_end)
+
+    handler = DBHandler()
+
+    start_event = PresenceStartEvent(0, "Cam1", None)
+    handler.handle(start_event)
+
+    end_event = PresenceEndEvent(0, "Cam1")
+    handler
+
+    assert fake_start_called == ["Cam1"]
+    assert fake_end_called[0][0] == 123
